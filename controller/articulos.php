c8836a34effa7c412991c702e40170801282022c
Jonathan-Caballero <jicaballero@unah.hn>
2021-11-15 19:42:05 -0600

    Commit con los accesos

--- controller/articulos.php
+++ controller/articulos.php
@@ -1,5 +1,15 @@
 <?php
-    header('Content-Type: application/json');
+     if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+        header('Access-Control-Allow-Origin: *');
+        header('Access-Control-Allow-Methods: POST, GET, DELETE, PUT, PATCH, OPTIONS');
+        header('Access-Control-Allow-Headers: token, Content-Type');
+        header('Access-Control-Max-Age: 1728000');
+        header('Content-Length: 0');
+        header('Content-Type: text/plain');
+        die();
+      }
+        header('Access-Control-Allow-Origin: *');  
+        header('Content-Type: application/json');
 
     require_once("../config/conexion.php");
     require_once("../models/Articulos.php");
